0a54551e196bb043badf26c9ee4dee994ab89099
climc118 <cxl5524@psu.edu>
2019-01-01 21:25:54 -0500

    transition logs to separate app

--- surface/urls.py
+++ surface/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.urls import path
 
-from surface.views import SurfaceHomeView
+from surface.views import SurfaceHomeView, ViewSurface
 
 app_name = "surface"
 urlpatterns = [

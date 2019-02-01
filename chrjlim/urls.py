8745f3b299313184fa0851513d8edd14250d09b8
climc118 <cxl5524@psu.edu>
2019-02-01 11:52:19 -0500

    set base url

--- chrjlim/urls.py
+++ chrjlim/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cavern/', include('cavern.urls')),
-    path('surface/', include('surface.urls'))
+    path('surface/', include('surface.urls')),
+    path('', RedirectView.as_view(url='surface/home'))
 ]
